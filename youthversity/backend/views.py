87f87a275d802acd8a0c5a093416141b98de49c9
noah04 <30809884+noah04@users.noreply.github.com>
2020-03-21 15:42:34 +0100

    implement Topics view

--- youthversity/backend/views.py
+++ youthversity/backend/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from .models import Subject
+
+
 # Create your views here.
 def feed(request):
     context = {}
     return render(request, 'feed.html', context)
+
+
+def topics(request):
+    context = {"subjects": list([s.name for s in Subject.objects.all()])}
+    return render(request, 'topics.html', context)
