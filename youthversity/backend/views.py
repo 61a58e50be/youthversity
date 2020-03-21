e5e75f1ccf4c7d25eec2f49907f8ecaf18ca678c
Jakob <no_user@mnet-mail.de>
2020-03-21 16:20:14 +0100

    trying to merge master changes in local work

--- youthversity/backend/views.py
+++ youthversity/backend/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from youthversity.backend.models import Post, Comment, CommentReply
 # Create your views here.
 def feed(request):
     context = {}
     return render(request, 'feed.html', context)
+
+def projects_id(request):
+    url=request.path
+    id=url.rsplit('/',1)[-1]
