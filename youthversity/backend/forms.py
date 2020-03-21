8b91763e58f71b1a24fd2f1f1ed9055058c7832e
noah04 <30809884+noah04@users.noreply.github.com>
2020-03-21 19:23:10 +0100

    add form for user registration

---
+++ youthversity/backend/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    name = forms.CharField(max_length=30, required=True, help_text='Not optional.')
+
+    class Meta:
+        model = User
+        fields = (
+            "name",
+        )
--- youthversity/backend/views.py
+++ youthversity/backend/views.py
@@ -1,8 +1,28 @@
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Subject, Post
+from .models import Subject, Post, User
+from .forms import SignUpForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('name')
+            print(username)
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            print(user)
+            login(request, user)
+
+            return redirect('')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
 
 
 # Create your views here.
