69644272036fced0a3c43f196553dddcfc389eeb
noah04 <30809884+noah04@users.noreply.github.com>
2020-03-21 17:15:34 +0100

    add landingpage

--- youthversity/backend/urls.py
+++ youthversity/backend/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('feed/', views.feed, name='feed'),
     path('topics/', views.topics, name='topics'),
--- youthversity/backend/views.py
+++ youthversity/backend/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -5,16 +6,26 @@ from .models import Subject, Post
 
 
 # Create your views here.
+@login_required
+def index(request):
+    user = request.user.id
+    context = dict(user=user)
+    return render(request, 'index.html', context=context)
+
+
+@login_required
 def feed(request):
     context = {}
     return render(request, 'feed.html', context)
 
 
+@login_required
 def topics(request):
     context = {"subjects": Subject.objects.all()}
     return render(request, 'topics.html', context)
 
 
+@login_required
 def projects_filter(request):
     if request.method != 'GET':
         # Todo: implement an error page
