bfd48de3b69d428721a58223c39afa522af7e169
noah04 <30809884+noah04@users.noreply.github.com>
2020-03-21 16:40:13 +0100

    implement view to filter posts by topic

--- youthversity/backend/admin.py
+++ youthversity/backend/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import Post
+
+
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    fields = ["subject",
+              "title",
+              "visibility",
+              "content",
+              "author",
+              "edited",
+              "type",
+              "upvotes",
+              ]
+
+
+admin.site.register(Post, PostAdmin)
--- youthversity/backend/views.py
+++ youthversity/backend/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Subject
+from .models import Subject, Post
 
 
 # Create your views here.
@@ -12,3 +13,21 @@ def feed(request):
 def topics(request):
     context = {"subjects": Subject.objects.all()}
     return render(request, 'topics.html', context)
+
+
+def projects_filter(request):
+    if request.method != 'GET':
+        # Todo: implement an error page
+        return HttpResponse('400 - Bad Request')
+
+    topics = request.GET.get('topics')
+
+    if topics:
+        filtered = Post.objects.filter(pk__in=topics)
+        context = dict(
+            posts=filtered,
+            query=list(request.GET.items())
+        )
+        return render(request, 'projects_filter.html', context)
+
+    return HttpResponse('400 - Bad Request')
