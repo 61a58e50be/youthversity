34126ddf4ed17068d7122a9b7211980117f2f356
caschnu <caschnu@Desktop>
2020-03-21 12:46:28 +0100

    first try on feed page

--- youthversity/backend/urls.py
+++ youthversity/backend/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('feed', views.feed, name='feed'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
