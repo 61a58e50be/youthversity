11e891c0002e6904d65ad533060f12bf1df7c780
caschnu <caschnu@Desktop>
2020-03-22 12:31:16 +0100

    added email address for user

--- youthversity/backend/forms.py
+++ youthversity/backend/forms.py
@@ -4,10 +4,11 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length=30, required=True, help_text='Not optional.')
+    username = forms.CharField(max_length=30, required=True, help_text='Required')
 
     class Meta:
         model = User
         fields = (
             "username",
+            "email",
         )
