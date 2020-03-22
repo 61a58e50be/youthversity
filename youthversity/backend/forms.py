3c1fc8c3aae5565796848c981784be53267ed957
David <a@a.a>
2020-03-22 15:59:20 +0100

    Feed and projects-new-feature with placeholder templates

--- youthversity/backend/forms.py
+++ youthversity/backend/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import ViolationReport
+from .models import ViolationReport, Subject, Post
 
 class SignUpForm(UserCreationForm):
     username = forms.CharField(max_length=30, required=True, help_text='Required')
@@ -26,3 +26,50 @@ class ReportForm(forms.Form):
 
 class CommentCreationForm(forms.Form):
     content = forms.CharField(label='Dein Kommentar', max_length=1000)
+
+
+class ProjectForm(forms.Form):
+    title = forms.CharField(label='Titel', max_length=100)
+
+    subjectChoices = [('null',"Naturwissenschaften"),
+                      ("Physik","Physik"),
+                      ("Chemie","Chemie"),
+                      ("Mathe","Mathe"),
+                      ("Biologie","Biologie"),
+                      ("Sonstige","Sonstige"),
+                      ("null","Sprachen"),
+                      ("Englisch","Englisch"),
+                      ("Deutsch","Deutsch"),
+                      ("Französisch","Französisch"),
+                      ("Spanisch","Spanisch"),
+                      ("Latein","Latein"),
+                      ("Altgriechisch","Altgriechisch"),
+                      ("Sonstige","Sonstige"),
+                      ("null","Gesellschaft"),
+                      ("Geschichte","Geschichte"),
+                      ("Politik","Politik"),
+                      ("Wirtschaft","Wirtschaft"),
+                      ("Geographie","Geographie"),
+                      ("Sozialkunde","Sozialkunde"),
+                      ("Sonstige","Sonstige"),
+                      ("null","Geisteswissenschaften"),
+                      ("Philosophie","Philosophie"),
+                      ("Religion","Religion"),
+                      ("null","Technik"),
+                      ("Informatik","Informatik"),
+                      ("Sonstige","Sonstige")]
+    
+    #subjectChoices = []
+    #for subject in Subject.objects.all():
+        #subjectChoices += [(subject, subject.name)]
+    subject = forms.CharField(label='Thema', widget=forms.Select(choices=subjectChoices))
+    content = forms.CharField(label='Text', widget=forms.Textarea)
+    file = forms.FileField(required=False)
+    
+    class Meta:
+        model = Post
+        fields = (
+            "title",
+            "subject",
+            "content",
+        )
